3a23419868cd1f75eb41447b017e5c99d284c0e9
stebaratheon <1705068@ugrad.cse.buet.ac.bd>
2020-11-03 01:29:12 +0600

    Accordion added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router , Route } from 'react-router-dom';
 import {JumbotronContainer} from './containers/jumbotron'
 import { FooterContainer } from './containers/footer';
+import { FaqsContainer } from './containers/faqs';
+import * as ROUTES from './constants/routes';
 
 function App() {
   return (
-    <>
-      <JumbotronContainer/>
-      <FooterContainer/>
-    </>
+    <Router>
+      <Route exact path={ROUTES.HOME}>
+        <JumbotronContainer/>
+        <FaqsContainer/>
+        <FooterContainer/>
+      </Route>
+    </Router>
   );
 }
 
