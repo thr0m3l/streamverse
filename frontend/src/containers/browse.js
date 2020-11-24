84fdfda759e72972034a9a646da09d3143fd99d5
stebaratheon <1705068@ugrad.cse.buet.ac.bd>
2020-11-24 23:12:14 +0600

    added card component(not significant for api)

--- frontend/src/containers/browse.js
+++ frontend/src/containers/browse.js
@@ -4,70 +4,95 @@ import { Card, Header, Loading, Player } from '../components';
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
 import { FooterContainer } from './footer';
+import {AuthContext} from './../context/auth-context';
 
 export function BrowseContainer({ slides }) {
-  const [category, setCategory] = useState('series');
-  const [profile, setProfile] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [slideRows, setSlideRows] = useState([]);
+    const [category, setCategory] = useState('series');
+    const [profile, setProfile] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [slideRows, setSlideRows] = useState([]);
 
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, [profile.displayName]);
-
-  useEffect(() => {
-    setSlideRows(slides[category]);
-  }, [slides, category]);
-
-  useEffect(() => {
-    //const fuse = new Fuse(slideRows, { keys: ['data.description', 'data.title', 'data.genre'] });
-    //const results = fuse.search(searchTerm).map(({ item }) => item);
-
-  }, [searchTerm]);
-
-  return  (
-    <>
-      
-      <Header src="joker1" dontShowOnSmallViewPort>
-        <Header.Frame>
-          <Header.Group>
-            <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
-            <Header.TextLink active={category === 'series' ? 'true' : 'false'} onClick={() => setCategory('series')}>
-              Series
-            </Header.TextLink>
-            <Header.TextLink active={category === 'films' ? 'true' : 'false'} onClick={() => setCategory('films')}>
-              Films
-            </Header.TextLink>
-          </Header.Group>
-          <Header.Group>
-            <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-            <Header.Profile>
-              <Header.Dropdown>
-                <Header.Group>
-                  <Header.TextLink></Header.TextLink>
-                </Header.Group>
-              </Header.Dropdown>
-            </Header.Profile>
-          </Header.Group>
-        </Header.Frame>
-
-        <Header.Feature>
-          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
-          <Header.Text>
-            Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham
-            City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a
-            futile attempt to feel like he's part of the world around him.
-          </Header.Text>
-          <Header.PlayButton>Play</Header.PlayButton>
-        </Header.Feature>
-      </Header>
     
-      <FooterContainer />
-    </>
-  
-  );
+    const auth = useContext(AuthContext); //auth context
+    console.log(auth.email);
+
+    useEffect(() => {
+      setTimeout(() => {
+        setLoading(false);
+      }, 3000);
+    }, [profile.displayName]);
+
+    useEffect(() => {
+      setSlideRows(slides[category]);
+      console.log("Slides from browse",slideRows);
+    }, [slides, category]);
+
+    useEffect(() => {
+      //const fuse = new Fuse(slideRows, { keys: ['data.description', 'data.title', 'data.genre'] });
+      //const results = fuse.search(searchTerm).map(({ item }) => item);
+
+    }, [searchTerm]);
+
+    return  (
+      <>
+        
+        <Header src="joker1" dontShowOnSmallViewPort>
+          <Header.Frame>
+            <Header.Group>
+              <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+              <Header.TextLink active={category === 'series' ? 'true' : 'false'} onClick={() => setCategory('series')}>
+                Series
+              </Header.TextLink>
+              <Header.TextLink active={category === 'films' ? 'true' : 'false'} onClick={() => setCategory('films')}>
+                Films
+              </Header.TextLink>
+            </Header.Group>
+            <Header.Group>
+              <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+              <Header.Profile>
+                <Header.Dropdown>
+                  <Header.Group>
+                    <Header.TextLink></Header.TextLink>
+                  </Header.Group>
+                </Header.Dropdown>
+              </Header.Profile>
+            </Header.Group>
+          </Header.Frame>
+
+          <Header.Feature>
+            <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+            <Header.Text>
+              Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham
+              City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a
+              futile attempt to feel like he's part of the world around him.
+            </Header.Text>
+            <Header.PlayButton>Play</Header.PlayButton>
+          </Header.Feature>
+        </Header>
+
+        <Card.Group>
+            {slideRows.map((slideItem)=>(
+              <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+                <Card.Title>{slideItem.title}</Card.Title>
+                <Card.Entities>
+              {slideItem.data.map((item) => (
+                <Card.Item key={item.docId} item={item}>
+                  <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                  <Card.Meta>
+                    <Card.SubTitle>{item.title}</Card.SubTitle>
+                    <Card.Text>{item.description}</Card.Text>
+                  </Card.Meta>
+                </Card.Item>
+              ))}
+            </Card.Entities>
+            </Card>
+            ))}
+        </Card.Group>
+      
+        <FooterContainer />
+      </>
+    
+    );
 }
